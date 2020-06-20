68c1eaba15bf765eccb9eaf66b647609a5f8d2e0
Diaa AlHaref <diaa.alharef@gmail.com>
2020-06-21 01:25:16 +0200

    Adding ScreenShot Class with documentation

---
+++ src/main/java/utilities/ScreenShot.java
@@ -0,0 +1,36 @@
+package utilities;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class ScreenShot {
+
+    /**
+     * Method Create Directory Screenshot to store images for failure test
+     *
+     * @param driver         object driver of type WebDriver
+     * @param ScreenShotName Name of the Test.png
+     */
+    public static void CaptureScreenshots(WebDriver driver, String ScreenShotName) {
+        Path dest = Paths.get(".//ScreenShots//", ScreenShotName + ".png");
+        try {
+            Files.createDirectories(dest.getParent());  //Create Directory for ScreenShot images
+            FileOutputStream out = new FileOutputStream(dest.toString());   //Create object for Image file of type output stream
+            out.write(((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES)); //Write in file the image taken output bytes (.png)
+            out.close();
+
+        } catch (IOException e) {
+            HandleExceptions.IOExceptionHandling(e); //Handle Output file exception thrown
+            System.out.println("Exception While taking screenshots" + e.getMessage()); //Output message for exception
+        }
+    }
+
+
+}
